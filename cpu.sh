cf110d8fce4d3bfa988ea41335e38aca278e3350
vedhagsvp <vedhag230223@gmail.com>
2025-03-06 16:53:32 +0530

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@as.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@eu.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@us.catchthatrabbit.com:8008
